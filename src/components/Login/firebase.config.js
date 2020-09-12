0b06ba2a2c59d6b946225950eed0cb3db98408e9
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-09-12 23:25:47 +0600

    Apply firebase & done this site

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDznu9-Csj1TY-30gGwh519F5E3P85cH0k",
+    authDomain: "burj-al-arab-master.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-master.firebaseio.com",
+    projectId: "burj-al-arab-master",
+    storageBucket: "burj-al-arab-master.appspot.com",
+    messagingSenderId: "459713047946",
+    appId: "1:459713047946:web:37a5fb4979c0c68a93ec73"
+  };
+  export default firebaseConfig;
