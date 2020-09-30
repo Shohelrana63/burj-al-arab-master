0cdda3e07c34674777474996df4dc1d021604427
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-09-30 15:39:19 +0600

    introduce jwt token & get it on client side

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,43 +2,54 @@ import React, { useContext } from 'react';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
-import{UserContext} from '../../App';
+import { UserContext } from '../../App';
 import { useHistory, useLocation } from 'react-router-dom';
 
 
 const Login = () => {
-    const[loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     const history = useHistory();
     const location = useLocation();
 
     let { from } = location.state || { from: { pathname: "/" } };
 
-    if(firebase.apps.length === 0){
+    if (firebase.apps.length === 0) {
         firebase.initializeApp(firebaseConfig);
     }
-    const handleGoogleSignIn = () =>{
-        
+    const handleGoogleSignIn = () => {
+
         var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-            
-            const{displayName, email} = result.user;
-            const signedInUser = {name: displayName, email}
-           setLoggedInUser(signedInUser);
-           history.replace(from);
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+
+            const { displayName, email } = result.user;
+            const signedInUser = { name: displayName, email }
+            setLoggedInUser(signedInUser);
+            storeAuthToken();
+            history.replace(from);
             // ...
-          }).catch(function(error) {
+        }).catch(function (error) {
             // Handle Errors here.
-            
+
             var errorMessage = error.message;
             console.log(errorMessage);
-          
-          });
+
+        });
+    }
+
+    const storeAuthToken = () => {
+        firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
+            .then(function (idToken) {
+                // console.log(idToken);
+                sessionStorage.setItem('token', idToken);
+            }).catch(function (error) {
+                // Handle error
+            });
     }
 
     return (
         <div>
             <h1>This is Login</h1>
-        <button onClick={handleGoogleSignIn}>google sign in</button>
+            <button onClick={handleGoogleSignIn}>google sign in</button>
         </div>
     );
 };
